edcd0daac2c87784aaf0040058e5ece4f98607cb
Suvam Patra <57254290+SuvamPatra0105@users.noreply.github.com>
2024-12-22 09:40:53 +0000

    Scope

---
+++ JS/scopes.js
@@ -0,0 +1,72 @@
+if(true) {
+    let a = 10;
+    const b = 5;
+    var c = 8;
+}
+
+console.log(a)      // ReferenceError: a is not defined.. block scope of a (let)
+console.log(b)      // ReferenceError: b is not defined.. block scope of b (const)
+console.log(c)      // 8... global scope of c (var)
+
+
+// Hoisting
+/* Variable Hoisting */
+console.log(d);     // undefined.. accessible but not initialized
+var d
+
+console.log(p);     // ReferenceError: Cannot access 'p' before initialization.. temporal dead zone
+let p
+
+console.log(w);     // ReferenceError: Cannot access 'w' before initialization.. temporal dead zone
+const w = 5
+
+
+/* Function Hoisting */
+
+// Function declaration
+check()                 // hi... function declaration and definition is hoisted to top
+function check() {
+    console.log('hi')
+}
+check()                 // hi
+
+
+// Function expression
+console.log(check2)              // undefined.. only variable declaration is hoisted, not func body/expression
+var check2 = function () {
+    console.log('hi')
+}
+check2()                        // hi
+
+//  alternatively,
+
+console.log(check2)                
+// ReferenceError: Cannot access 'check2' before initialization.. since const variable
+const check2 = function () {
+    console.log('hi')
+}
+check2()                // hi
+
+// Class Hoisting
+console.log(new Car())       
+/* ReferenceError: Cannot access 'Car' before initialization...
+"temporal dead zone" like let, const */
+class Car {
+    constructor() {
+        console.log('hello')
+    }
+}
+console.log(new Car())
+
+
+
+function calc() {
+    let a = 10;             // block scope
+    const b = 5;            // block scope
+    var c = 8;              // function scope
+}
+
+calc()
+console.log(a)      // ReferenceError: a is not defined.. block scope of a (let)
+console.log(b)      // ReferenceError: b is not defined.. block scope of b (const)
+console.log(c)        // undefined (due to hoisting with var)
