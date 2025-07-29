0d594fa978f136afee8f9c3a2b517fad1b39a1a1
Suvam Patra <57254290+SuvamPatra0105@users.noreply.github.com>
2025-07-29 12:55:05 +0530

    Update Currying.js

--- JS/Currying.js
+++ JS/Currying.js
@@ -1,4 +1,16 @@
 // Function Currying
+Definition - Function currying is a functional programming technique where a function with multiple arguments is transformed into a sequence of functions, each taking a single argument.
+
+
+Normal function - 
+
+function a(x,y) {
+   return x + y;
+}
+
+console.log(a(1,2))
+
+Function currying -
 
 function a(x) {
    return function b(y) {
@@ -18,7 +30,7 @@ function a1(x) {
      }
  }
  
- console.log(a1(1)(2)())        // 3
+ console.log(a1(1)(2)())        // 3... last empty parentheses is imp here
 
 
  // Usecase
@@ -48,8 +60,9 @@ function a1(x) {
 
 
  // Infinite Currying
-
- function a2(x) {
+Def - Infinite function currying allows you to keep calling a function repeatedly with one argument at a time — until a final call (typically with no argument or a special token) tells it to compute the result.
+ 
+function a2(x) {
     return function b2(y) {
         if (y) 
             return a2(x+y)
@@ -58,5 +71,5 @@ function a1(x) {
     }
  }
 
- console.log(a2(1)(2)(3)(4)())     // 10
+ console.log(a2(1)(2)(3)(4)())     // 10.. dynamically increase arguments, it will not give error
  // Make sure to use () at the last while invoking since "return a2(x+y)" is calling a2() again and at last by using (), we are not providing any value to b2(), i.e. y is undefined, which results in returning x, i.e. the total sum accumulated
